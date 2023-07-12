cf932bf697b0229f6e3c2804ad3eccb8edc2dab8
Maicon Douglas <11251303+mikedpsm@users.noreply.github.com>
2023-07-12 00:21:39 -0300

    Add project files.

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesApi.Models;
+
+namespace MoviesApi.Controllers;
+
+[ApiController]
+[Route("controller")]
+public class MovieController : ControllerBase
+{
+    private static List<Movie> movies = new List<Movie>();
+
+    [HttpPost]
+    public void AddMovie([FromBody] Movie movie)
+    {
+        movies.Add(movie);
+        Console.WriteLine(movie.Title);
+        Console.WriteLine(movie.Duration);
+    }
+
+    [HttpGet]
+    public List<Movie> GetAllMovies()
+    {
+        return movies;
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoviesApi.Models;
+
+public class Movie
+{
+
+    [Required(ErrorMessage = "Movie title is required")]
+    public string Title { get; set; }
+    [Required(ErrorMessage = "Movie genre is required")]
+    [MaxLength(50, ErrorMessage = "Movie genre may not exceed 50 chars")]
+    public string Genre { get; set; }
+    [Required]
+    [Range(70, 600, ErrorMessage = "Duration must be between 70 and 600 minutes")]
+    public int Duration { get; set; }
+}
